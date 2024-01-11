87657df4385bf8519436085b73f54f50e62678c5
corey clarke <sombas100@yahoo.com>
2024-01-11 23:36:32 +0000

    Showing of the issues page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -22,7 +22,7 @@ const IssuesPage = async () => {
               Status
             </Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell className="hidden md:table-cell">
-              created
+              Created
             </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
