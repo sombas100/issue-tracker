68ca25c39da7857b2b70d44765ca6e5b0e7d61bc
corey clarke <sombas100@yahoo.com>
2024-01-15 20:12:21 +0000

    Show a confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,7 +1,40 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogDescription,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button,
+  Flex,
+} from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialogTrigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialogTrigger>
+      <AlertDialog.Content>
+        <AlertDialogTitle>Confirm Deletion</AlertDialogTitle>
+        <AlertDialogDescription>
+          Are you sure you want to delete this issue? This action cannot be
+          reversed.
+        </AlertDialogDescription>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialogAction>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialogAction>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteIssueButton;
